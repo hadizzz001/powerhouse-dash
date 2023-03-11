c8d2a511f57af405c9a3053f0f4222206550c56a
vito medlej <vito.medlej@gmail.com>
2023-03-11 19:52:47 +0300

    deleted auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,9 +5,9 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
-import { AuthConsumer, AuthProvider } from '../contexts/auth-context';
+// import { AuthConsumer, AuthProvider } from '../contexts/auth-context';
 import { createEmotionCache } from '../utils/create-emotion-cache';
-import { registerChartJs } from '../utils/register-chart-js';
+// import { registerChartJs } from '../utils/register-chart-js';
 import { theme } from '../theme';
 import '../../src/styles/styles.css'
 registerChartJs();
