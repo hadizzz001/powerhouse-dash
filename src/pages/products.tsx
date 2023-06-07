d729eb36868492b085e79597397c756963253e19
Vito Medlej <vito.medlej@gmail.com>
2023-06-07 19:08:44 +0300

    Update products.tsx

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -104,8 +104,7 @@ const Page = () => {
                   <Box sx={{width:'100%'}}>
                     <Box sx={{width:'100%',background:'white',height:'300px'}}>
 
-<!--                     <img src={`https://optinmonster.com/wp-content/uploads/2019/03/wordpress-maintenance-mode-coming-soon-plugins.png`} className='img cover' alt=''/>
-                       -->
+
 <img src={product?.images[0] || ''} className='img cover' alt=''/>                      
                     </Box>
                     <Box sx={{px:'.35em',py:'.5em',display:'flex',justifyContent:'space-between'}}>
